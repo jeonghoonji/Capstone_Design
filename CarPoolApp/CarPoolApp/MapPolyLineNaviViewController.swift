54bcf9ce5ee76e6c176b6f62ddfb617b3b837809
jeonghoonji <wlwjdgns0920@naver.com>
2022-09-17 22:55:38 +0900

    맵메뉴 개선

---
+++ CarPoolApp/CarPoolApp/MapPolyLineNaviViewController.swift
@@ -0,0 +1,69 @@
+//
+//  MapMenuTestViewController.swift
+//  CarPoolApp
+//
+//  Created by 지정훈 on 2022/09/17.
+//
+
+import UIKit
+import NMapsMap
+import SwiftyJSON
+import Alamofire
+import CoreLocation
+
+
+
+
+class MapPolyLineNaviViewController:UIViewController,CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: UIView!
+
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let naverMapView = NMFMapView(frame: view.frame)
+                view.addSubview(naverMapView)
+//        naverMapView.showCompass = true
+//        naverMapView.showLocationButton = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+                
+        if CLLocationManager.locationServicesEnabled() {
+            print(" MapPolyLineNaviViewController 위치 서비스 On 상태")
+            locationManager.startUpdatingLocation()
+//            print(locationManager.location?.coordinate)
+                    
+                    
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+            cameraUpdate.animation = .easeIn
+            naverMapView.moveCamera(cameraUpdate)
+            
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+            marker.mapView = naverMapView
+            
+//            let endPointMarker = NMFMarker()
+//            endPointMarker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+//            endPointMarker.mapView = naverMapView
+//
+            } else {
+                print("위치 서비스 Off 상태")
+                }
+        
+            
+           
+
+    
+        }
+    
+    
+    //
+}
+
+
+
+
