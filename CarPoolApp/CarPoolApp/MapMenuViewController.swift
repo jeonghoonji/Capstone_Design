46a5af475adc5cd4c2ab2613970c21da37e4bd66
jeonghoonji <wlwjdgns0920@naver.com>
2022-10-20 01:46:17 +0900

    카카오 Rest-API(자동차길찾기), 10월 20일까지 작업 내용

--- CarPoolApp/CarPoolApp/MapMenuViewController.swift
+++ CarPoolApp/CarPoolApp/MapMenuViewController.swift
@@ -17,25 +17,39 @@ import SwiftyJSON
 import Alamofire
 import CoreLocation
 
-
 class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
     
 
     @IBOutlet weak var startPointTextField: UITextField!
     @IBOutlet weak var endPointTextField: UITextField!
-    @IBOutlet weak var getDirectionButton: UIButton!
     @IBOutlet weak var mapView: UIView!
-    @IBOutlet weak var currentButton: UIButton!
+    
 
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
     var locationManager = CLLocationManager()
     
+    struct Addresses: Decodable{
+        var x : Double = 0.0
+        var y : Double = 0.0
+        
+        init(x: Double, y: Double) {
+            self.x = x
+            self.y = y
+        }
+    }
+   
+ 
     
-    
-    //
+    //깃허브에는 숨길 코드
+   
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         //위치 정보 묻기
         locationManager.delegate = self
@@ -47,12 +61,26 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
         let header2 = HTTPHeader(name: "X-NCP-APIGW-API-KEY", value: NAVER_CLIENT_SECRET)
         let headers = HTTPHeaders([header1,header2])
         
-        let starText: String
+
+    
         
-        let naverMapView = NMFNaverMapView(frame: view.frame)
+        let naverMapView = NMFMapView(frame: view.frame)
                 mapView.addSubview(naverMapView)
-        naverMapView.showCompass = true
-        naverMapView.showLocationButton = true
+
+        //현재 위치로 이동후 마커 표시 ( 출발지 )
+        if CLLocationManager.locationServicesEnabled() {
+            print(" MapPolyLineNaviViewController 위치 서비스 On 상태")
+            locationManager.startUpdatingLocation()
+            //            print(locationManager.location?.coordinate)
+            
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+            cameraUpdate.animation = .easeIn
+            naverMapView.moveCamera(cameraUpdate)
+            
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+            marker.mapView = naverMapView
+        }
 
         
         
@@ -63,6 +91,8 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
         // user_latitude, uesr_longitude 을 String형으로 변환해서 쿼리문 만들기
         let reverseURL :String = NAVER_REVERSE_GEOCODE_URL + String(uesr_longitude) + "," + String(user_latitude) + "&sourcecrs=epsg:4326&output=json&orders=legalcode"
         
+        
+                   
         AF.request(reverseURL, method: .get ,headers: headers ).validate()
                .responseJSON { response in
                    switch response.result {
@@ -71,19 +101,18 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
                        
                        let results = json["results"]
                        let region = results[0]["region"]
+                       
                        // 경기도,서울시,부산시,강원도 등등
                        let area1 = region["area1"]
                        var city = area1["name"]
-                       print(city)
+
                        // 화성시,수원시,용인시 등등
                        let area2 = region["area2"]
                        var si = area2["name"]
-                       print(si)
+                       
                        // 석우동, 동탄1동, 반석동 등등
                        let area3 = region["area3"]
                        var dong = area3["name"]
-                       print(dong)
-                       
                        
                        self.startPointTextField.text = "\(city) \(si) \(dong)"
                     case .failure(let error):
@@ -92,8 +121,9 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
                        fatalError()
                    }
                }
-    
         
+       
+
 
     //
     }
@@ -101,22 +131,37 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
     @IBAction func getDirectionButtonTapped(_ sender: UIButton) {
         let station = endPointTextField.text
         guard let encodeAddress = station?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! else { return  }
-        
-        
+
+        //숨겨야할 코드
         let header1 = HTTPHeader(name: "X-NCP-APIGW-API-KEY-ID", value: NAVER_CLIENT_ID)
         let header2 = HTTPHeader(name: "X-NCP-APIGW-API-KEY", value: NAVER_CLIENT_SECRET)
         let headers = HTTPHeaders([header1,header2])
-        
-        
+        print("headers: \(headers)")
+        //폴리메뉴로 도착지 한글 보내기
+        let endString = (self.endPointTextField.text)!
+        UserDefaults.standard.setValue(endString, forKey: "endPointTextUserDefaults")
+
         AF.request(NAVER_GEOCODE_URL + encodeAddress, method: .get,headers: headers).validate()
                .responseJSON { response in
                    switch response.result {
-                   case .success(let value as [String:Any]):
+                   case .success(let value /*as [String:Any]*/):
+
                        let json = JSON(value)
                        let data = json["addresses"]
-                       let lat = data[0]["y"]
-                       let lon = data[0]["x"]
-                       print("\(self.endPointTextField.text)","위도는",lat,"경도는",lon)
+                       let detail = data[0]
+
+                       let lan = detail["y"]
+                       let lon = detail["x"]
+                       print("detail\(detail)")
+//                       print("lan: \(lan)")
+//                       print("lan Type:\(type(of:lan))")
+//                       print("lan: \(lon)")
+                       
+//                       let test : Addresses = Addresses(x: lan.rawValue as! Double, y: lon.rawValue as! Double)
+//                       print(test)
+
+               
+                       print("lon Type:\(type(of:lon))")
                    case .failure(let error):
                        print(error.errorDescription ?? "")
                    default :
@@ -124,18 +169,34 @@ class MapMenuViewController:UIViewController,CLLocationManagerDelegate{
                    }
                }
         
-//
-    
-    }
-    
-    
-    @IBAction func currentButtonTapped(_ sender: Any) {
-        print("button tapped")
-        
-    }
-    
-    
     //
+    }
+
+
+    
+    @IBAction func testButton(_ sender: Any) {
+        //
+        let header1 = HTTPHeader(name: "Authorization", value: KAKAO_API_KEY)
+        let kakaoHeaders = HTTPHeaders([header1])
+        print("kakaoHeaders: \(kakaoHeaders)")
+        AF.request(KAKAO_URL,method: .get , headers: kakaoHeaders).validate()
+            .responseJSON{ response in
+                switch response.result {
+                case .success(let value):
+                    print("KAKAO SUCCESS")
+                    print(value)
+                    
+                    
+                case .failure(let error):
+                    print("KAKAO FAIL")
+                    print(error.errorDescription ?? "" )
+                default:
+                    fatalError()
+                }
+            }
+    }
+    
+//
 }
 
 
--- CarPoolApp/CarPoolApp/MapPolyLineNaviViewController.swift
+++ CarPoolApp/CarPoolApp/MapPolyLineNaviViewController.swift
@@ -16,17 +16,33 @@ import CoreLocation
 
 class MapPolyLineNaviViewController:UIViewController,CLLocationManagerDelegate{
     
+  
+    
+    var test1 : Double = 0.0
+    var test2 : Double = 0.0
+    
     @IBOutlet weak var mapView: UIView!
 
     var locationManager = CLLocationManager()
     
+    let endString = UserDefaults.standard.string(forKey:"endPointTextUserDefaults")!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("endString: \(endString)")
+    
         let naverMapView = NMFMapView(frame: view.frame)
                 view.addSubview(naverMapView)
 //        naverMapView.showCompass = true
 //        naverMapView.showLocationButton = true
+        
+        //원하는 좌표 마커
+        // 이마트 좌표
+        let marker1 = NMFMarker()
+        marker1.position = NMGLatLng(lat: 37.2143049, lng: 127.0795158)
+        marker1.mapView = naverMapView
+        
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -35,35 +51,31 @@ class MapPolyLineNaviViewController:UIViewController,CLLocationManagerDelegate{
         if CLLocationManager.locationServicesEnabled() {
             print(" MapPolyLineNaviViewController 위치 서비스 On 상태")
             locationManager.startUpdatingLocation()
-//            print(locationManager.location?.coordinate)
-                    
-                    
+            //            print(locationManager.location?.coordinate)
+            
+            
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
             cameraUpdate.animation = .easeIn
             naverMapView.moveCamera(cameraUpdate)
             
+            //현재 위치 마커
             let marker = NMFMarker()
             marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
             marker.mapView = naverMapView
-            
-//            let endPointMarker = NMFMarker()
-//            endPointMarker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
-//            endPointMarker.mapView = naverMapView
-//
-            } else {
-                print("위치 서비스 Off 상태")
-                }
+            }
+        //
+        let header1 = HTTPHeader(name: "X-NCP-APIGW-API-KEY-ID", value: NAVER_CLIENT_ID)
+        let header2 = HTTPHeader(name: "X-NCP-APIGW-API-KEY", value: NAVER_CLIENT_SECRET)
+        let headers = HTTPHeaders([header1,header2])
         
-            
-           
-
-    
+        print("headers:\(headers)")
+        
+        
+        //viewdidLoad
         }
-    
-    
-    //
-}
 
+//class
+}
 
 
 
